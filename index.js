ef30737f8945be946528abf61778e02fbe0a755e
최준근 <58229545+JunTree@users.noreply.github.com>
2021-04-30 18:11:38 +0900

    회원가입

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+const {User} = require("./models/User");
 const mongoose = require('mongoose')
+
+app.use(express.urlencoded());
+app.use(express.json());
+
 mongoose.connect('mongodb+srv://juntree:juntree1928@boilerplate.cy8kd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 } ).then(() => console.log('MongoDB Connected...'))
@@ -10,5 +14,24 @@ mongoose.connect('mongodb+srv://juntree:juntree1928@boilerplate.cy8kd.mongodb.ne
 
 
 app.get('/', (req, res) => { res.send('Hello World! ~~안녕하세요!!') })
+
+app.post('/register', (req, res)=>{
+    //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+    // 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err,userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+}) 
+
+
+
+
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
