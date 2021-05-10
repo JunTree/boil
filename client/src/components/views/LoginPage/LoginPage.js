e5dd85ddb1ab1ff825fba6401ef4534ef8835fa4
최준근 <58229545+JunTree@users.noreply.github.com>
2021-05-10 13:55:58 +0900

    component

---
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoginPage() {
+    return (
+        <div>
+            LoginPage 로그인 
+        </div>
+    )
+}
+
+export default LoginPage
---
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RegisterPage() {
+    return (
+        <div>
+            RegisterPage 레지스터
+        </div>
+    )
+}
+
+export default RegisterPage
